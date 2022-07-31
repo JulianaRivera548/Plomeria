8a59ffcc6677b63c00951dedeb772b44f0bef26f
Juliana Rivera <jriveras@correo.udistrital.edu.co>
2022-07-31 13:12:23 -0500

    Vistas

---
+++ Vista/ConsultarPlomeros.php
@@ -0,0 +1,78 @@
+<html >
+<head>
+
+<meta charset="UTF-8">
+
+<link rel="stylesheet"
+	href="https://use.fontawesome.com/releases/v5.11.1/css/all.css" />
+
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+	crossorigin="anonymous">
+
+<script
+	src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+	crossorigin="anonymous"></script>
+	
+	
+
+	
+	
+
+</head>
+
+<div class="container">
+	<div class="row mt-3">
+		<div class="col">
+			<div class="card">
+				<h5 class="card-header">Plomeros</h5>
+				<div class="card-body">
+
+
+					<table class="table table-striped table-hover">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Nombre</th>
+								<th>Certificados</th>
+								 <th >Servicios</th>
+								  <th >Estado</th>
+								 
+								 
+
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th scope="row">1</th>
+								<td>Mark</td>
+								<td>3</td>
+								<td>Todos</td>
+								<td>
+								<i class="fa fa-check" aria-hidden="true"></i>
+								<i class="fa fa-times" aria-hidden="true"></i>
+								
+
+								</td>
+								
+								
+							</tr>
+							<tr>
+						
+						</tbody>
+					</table>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+</div>
+
+</html>
